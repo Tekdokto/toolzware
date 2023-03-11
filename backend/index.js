accf36d4490f248a751b625ae1b725bb605ce71d
Doktogreen <ajiboduraphael@gmail.com>
2023-03-11 13:49:31 +0000

    added some sections to frontend

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require("cors")
+const mongoose = require("mongoose")
+
+const app = express()
+require("dotenv").config
+
+app.use(cors())
+app.use(express.json())
+
+
+const server = app.listen(process.env.PORT, () => {
+    console.log("Server is logged to", process.env.PORT)
+})
